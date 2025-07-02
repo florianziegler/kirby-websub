f648bf0b72754e00aa010d1f027543ec283415e1
Florian Ziegler <florian@haptiq.studio>
2025-07-02 13:16:05 +0200

    Add `changeSlug` and `changeTitle` hooks
    - Changing the slug or the title will now also trigger sending a websub update.
    - The plugin will now also manually flush the cache before sending the websub update, as the hooks run before Kirby itself flushes the caches.
    - Version bump to 1.1.0

--- index.php
+++ index.php
@@ -4,12 +4,25 @@ require_once __DIR__ . '/inc/send-update.php';
 Kirby::plugin( 'florianziegler/websub', [
 	'hooks' => [
 		'page.changeStatus:after' => function ( $newPage ) {
+			kirby()->cache( 'pages' )->flush();
 			if ( $newPage->isPublished() ) {
 				\florianziegler\websub\send_update();
 			}
 		},
-
+		'page.changeSlug:after' => function ( $newPage ) {
+			kirby()->cache( 'pages' )->flush();
+			if ( $newPage->isPublished() ) {
+				\florianziegler\websub\send_update();
+			}
+		},
+		'page.changeTitle:after' => function ( $newPage ) {
+			kirby()->cache( 'pages' )->flush();
+			if ( $newPage->isPublished() ) {
+				\florianziegler\websub\send_update();
+			}
+		},
 		'page.update:after' => function ( $newPage ) {
+			kirby()->cache( 'pages' )->flush();
 			if ( $newPage->isPublished() ) {
 				\florianziegler\websub\send_update();
 			}
